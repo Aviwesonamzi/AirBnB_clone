15b08e3fe3b514fca3874f8920b39a967f84ff82
root <you@example.com>
2024-07-11 19:33:12 -0700

    models

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module for BaseModel
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    A base class for other classes.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of BaseModel.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        Returns a string representation of the instance.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute `updated_at` with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        """
+        result = self.__dict__.copy()
+        result['__class__'] = self.__class__.__name__
+        result['created_at'] = self.created_at.isoformat()
+        result['updated_at'] = self.updated_at.isoformat()
+        return result
