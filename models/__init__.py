eb6709dc0cae55b7e6a6fc9feec8d668d0f98769
root <you@example.com>
2024-07-11 19:52:40 -0700

    f

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,9 @@
-# models/__init__.py
+#!/usr/bin/python3
+"""
+Init module for models package
+"""
 
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Module for BaseModel
+Module for BaseModel class
 """
 
 import uuid
 from datetime import datetime
-
+from models import storage  # Import the storage instance
 
 class BaseModel:
     """
@@ -26,6 +26,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)  # Add to storage when a new instance is created
+            storage.save()     # Save immediately
 
     def __str__(self):
         """
@@ -35,9 +37,11 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the public instance attribute `updated_at` with the current datetime.
+        Updates the public instance attribute `updated_at` with the current datetime
+        and saves the instance to storage.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
