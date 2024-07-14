9f9f1a16ebb3370a27d03e5c46aa1805ff9ca077
root <you@example.com>
2024-07-14 05:39:33 -0700

    console

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for HBNB."""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()  # Print a newline for a clean exit
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line input"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
