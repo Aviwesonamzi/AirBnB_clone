5609573e169fb19cf33a47f30b048592a58768d9
root <you@example.com>
2024-07-14 05:51:55 -0700

    documentation

--- console.py
+++ console.py
@@ -1,29 +1,65 @@
 #!/usr/bin/python3
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for HBNB."""
+    """
+    Command interpreter for HBNB.
+
+    Attributes:
+        prompt (str): The custom command prompt.
+    """
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """
+        Quit command to exit the program.
+
+        Args:
+            arg (str): The argument passed to the command (not used).
+
+        Returns:
+            bool: True to exit the command loop.
+        """
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
+        """
+        EOF command to exit the program.
+
+        Args:
+            arg (str): The argument passed to the command (not used).
+
+        Returns:
+            bool: True to exit the command loop.
+        """
         print()  # Print a newline for a clean exit
         return True
 
     def emptyline(self):
-        """Do nothing on empty line input"""
+        """
+        Do nothing on empty line input.
+
+        Returns:
+            None
+        """
         pass
 
     def default(self, line):
-        """Handle unknown commands"""
+        """
+        Handle unknown commands.
+
+        Args:
+            line (str): The input line.
+
+        Returns:
+            None
+        """
         if line.strip() == "":
             return
         print("*** Unknown syntax: {}".format(line))
 
 if __name__ == '__main__':
+    """
+    Entry point of the command interpreter.
+    """
     HBNBCommand().cmdloop()
