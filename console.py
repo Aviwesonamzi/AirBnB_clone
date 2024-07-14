c54468a571711294fbbcf3287825f71436510034
root <you@example.com>
2024-07-14 13:00:19 -0700

    cloning

--- console.py
+++ console.py
@@ -1,98 +1,99 @@
 #!/usr/bin/python3
-# console.py
-
+"""Defines the HBNB console."""
 import cmd
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models import storage
-# Add other necessary imports...
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for HBNB."""
-
+    """Defines the HBNB command interpreter."""
     prompt = '(hbnb) '
-    classes = {'BaseModel': BaseModel}
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'State': State,
+        'City': City, 'Amenity': Amenity, 'Place': Place, 'Review': Review
+    }
 
-    def do_create(self, args):
-        """Create a new instance of BaseModel, save it (to the JSON file), and print the id."""
-        if not args:
+    def do_create(self, arg):
+        """Creates a new instance of a class."""
+        if not arg:
             print("** class name missing **")
             return
-        try:
-            cls = globals()[args]
-        except KeyError:
+        if arg not in self.classes:
             print("** class doesn't exist **")
             return
-        new_instance = cls()
+        new_instance = self.classes[arg]()
         new_instance.save()
         print(new_instance.id)
 
-    def do_show(self, args):
-        """Prints the string representation of an instance based on the class name and id."""
-        args = args.split()
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        args = arg.split()
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in globals():
+        if args[0] not in self.classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         key = f"{args[0]}.{args[1]}"
-        try:
-            print(storage.all()[key])
-        except KeyError:
+        instance = storage.all().get(key)
+        if not instance:
             print("** no instance found **")
+            return
+        print(instance)
 
-    def do_destroy(self, args):
-        """Deletes an instance based on the class name and id (save the change into the JSON file)."""
-        args = args.split()
+    def do_destroy(self, arg):
+        """Deletes an instance."""
+        args = arg.split()
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in globals():
+        if args[0] not in self.classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         key = f"{args[0]}.{args[1]}"
-        try:
-            del storage.all()[key]
-            storage.save()
-        except KeyError:
+        if key not in storage.all():
             print("** no instance found **")
+            return
+        del storage.all()[key]
+        storage.save()
 
-    def do_all(self, args):
-        """Prints all string representation of all instances based or not on the class name."""
-        if not args:
-            for obj in storage.all().values():
-                print(obj)
-        else:
-            if args not in globals():
-                print("** class doesn't exist **")
-                return
-            for key, obj in storage.all().items():
-                if key.startswith(args):
-                    print(obj)
+    def do_all(self, arg):
+        """Prints all string representations of all instances."""
+        if arg and arg not in self.classes:
+            print("** class doesn't exist **")
+            return
+        instances = [
+            str(obj) for obj in storage.all().values()
+            if not arg or obj.__class__.__name__ == arg
+        ]
+        print(instances)
 
-    def do_update(self, args):
-        """Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)."""
-        args = args.split()
+    def do_update(self, arg):
+        """Updates an instance."""
+        args = arg.split()
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in globals():
+        if args[0] not in self.classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         key = f"{args[0]}.{args[1]}"
-        try:
-            obj = storage.all()[key]
-        except KeyError:
+        instance = storage.all().get(key)
+        if not instance:
             print("** no instance found **")
             return
         if len(args) < 3:
@@ -101,10 +102,21 @@ class HBNBCommand(cmd.Cmd):
         if len(args) < 4:
             print("** value missing **")
             return
-        attr_name = args[2]
-        attr_value = args[3].strip('"')
-        setattr(obj, attr_name, attr_value)
-        obj.save()
+        setattr(instance, args[2], args[3])
+        instance.save()
 
-if __name__ == "__main__":
+    def emptyline(self):
+        """Overrides the default behavior to do nothing."""
+        pass
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        print()
+        return True
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,19 +36,8 @@ class FileStorage:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
                     cls_name = value["__class__"]
-                    if cls_name == "BaseModel":
-                        self.__objects[key] = BaseModel(**value)
-                    elif cls_name == "User":
-                        self.__objects[key] = User(**value)
-                    elif cls_name == "State":
-                        self.__objects[key] = State(**value)
-                    elif cls_name == "City":
-                        self.__objects[key] = City(**value)
-                    elif cls_name == "Amenity":
-                        self.__objects[key] = Amenity(**value)
-                    elif cls_name == "Place":
-                        self.__objects[key] = Place(**value)
-                    elif cls_name == "Review":
-                        self.__objects[key] = Review(**value)
+                    cls = globals().get(cls_name)
+                    if cls:
+                        self.__objects[key] = cls(**value)
         except FileNotFoundError:
             pass
